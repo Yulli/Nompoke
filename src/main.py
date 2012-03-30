c8659f1620f11de5489a6c11339d9e8741a1dbeb
Yulli Equine <yulli@yulli.org>
2012-03-29 23:23:16 -0400

    Syntax error in checking for equality

--- src/main.py
+++ src/main.py
@@ -26,7 +26,7 @@ def load_png(name):
     except pygame.error, message:
         print 'Cannot load image:', fullname
         raise SystemExit, message
-    if scale = 2:                              # if the game scale is 2x
+    if scale == 2:                              # if the game scale is 2x
         return pygame.transform.scale2x(image) # double the image size
     else:                                      # otherwise,
         scaledsize = (pygame.Surface.get_width(image) * scale, pygame.Surface.get_height(image) * scale) # just scale the image by game scale
